98c626f2c9033fd158c9f4d8f7cc6ad87598e1e4
renan <renanmaziero34@gmail.com>
2025-06-18 21:29:14 -0300

    update branch
    add new functions

--- code/player.py
+++ code/player.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 import pygame
 from code.Entity import Entity
-from code.Const import ENTITY_SPEED 
+from code.Const import ENTITY_SHOT_DELAY, ENTITY_SPEED, PLAYER_KEY_SHOOT 
 from code.Const import PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT
 from code.Const import WIN_HEIGHT, WIN_WIDTH
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
-
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name] 
     def move(self, ):
         pressed_key = pygame.key.get_pressed() 
         #enquanto a posição da nave no retangulo(eixo y) for maior que 0, continua a subir, se for menor ele para na borda.
@@ -22,3 +23,11 @@ class Player(Entity):
             self.rect.centerx -= ENTITY_SPEED[self.name]
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH: 
             self.rect.centerx += ENTITY_SPEED[self.name]
+    
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery)) 
