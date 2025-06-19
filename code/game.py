296c3ded68f2aeac6fef1674548d7435ab7089b9
renan <renanmaziero34@gmail.com>
2025-06-18 21:40:49 -0300

    updade branch
    add comentaries

--- code/game.py
+++ code/game.py
@@ -9,12 +9,12 @@ from code.Menu import Menu
 class Game:
     def __init__(self,):
         pygame.init() #inicializa o pygame.
-        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT)) #cria uma janela com width = 800 e heigth = 500.
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT)) #cria uma janela do jogo com width = 800 e heigth = 500.
 
-    def run(self, ):
+    def run(self, ):# executa o game.
         while True: #manter janela em loop.
             #chama o menu.
-            menu = Menu(self.window)
+            menu = Menu(self.window)# chama menu.
             menu_return = menu.run()# executa o menu.
             
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:# traz a opçãos do menu.
