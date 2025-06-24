4b6f452dbd865b8141174c025f29efc6336b7003
renan <renanmaziero34@gmail.com>
2025-06-24 20:44:10 -0300

    upload codes

--- code/entity.py
+++ code/entity.py
@@ -3,7 +3,7 @@
 from abc import ABC, abstractmethod
 import pygame
 
-from code.Const import ENTITY_HEALTH
+from code.Const import ENTITY_HEALTH, ENTITY_DAMEGE, ENTITY_SCORE
 
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
@@ -12,6 +12,10 @@ class Entity(ABC):
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
         self.helth = ENTITY_HEALTH[self.name]
+        self.damege = ENTITY_DAMEGE[self.name]
+        self.score = ENTITY_SCORE[self.name]
+        self.last_dmg = 'none'
+    
     @abstractmethod
     def move(self, ):
         pass
--- code/level.py
+++ code/level.py
@@ -8,7 +8,7 @@ import pygame
 from pygame.font import Font
 from pygame import Surface, Rect
 
-from code.Const import COLOR_WHITE, SPAWN_TIME, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY
+from code.Const import COLOR_CYAN, COLOR_GREEN, COLOR_WHITE, SPAWN_TIME, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY
   
 from code.Enemy import Enemy
 from code.Entity import Entity
@@ -43,6 +43,10 @@ class Level:
                     shoot = ent.shoot()
                     if shoot is not None:
                         self.entity_list.append(shoot)
+                if ent.name == 'Player1':
+                    self.level_text(14, f'Player1 - Helth: {ent.helth} | Score: {ent.score}', COLOR_GREEN, (10, 25))
+                if ent.name == 'Player2':
+                    self.level_text(14, f'Player2 - Helth: {ent.helth} | Score: {ent.score}', COLOR_CYAN, (10, 45))
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -65,8 +69,4 @@ class Level:
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect  = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
-        self.window.blit(source=text_surf, dest=text_rect)     
-            
-            
-            
-            
+        self.window.blit(source=text_surf, dest=text_rect)           
